5b5b399fb6453e098e104a99fdde70736c2cfda4
noticesoftware <noticesoftware@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-09 18:45:24 +0000

    Version 2.0.3 Release
    git-svn-id: https://plugins.svn.wordpress.org/roost-for-bloggers/tags/2.0.3@872334 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ layout/js/roostjs.js
@@ -0,0 +1,11 @@
+    var _roost = _roost || [];
+    _roost.push(['appkey', pushNotificationsByRoostMe.appkey]);
+
+    !function(d,s,id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if(!d.getElementById(id)){
+            js=d.createElement(s); js.id=id;
+            js.src='//get.roost.me/js/roost.js';
+            fjs.parentNode.insertBefore(js,fjs);
+        }
+    }(document, 'script', 'roost-js');
