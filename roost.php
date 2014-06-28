19c5e54034d036d3a483c0cebafc1cce04089cea
noticesoftware <noticesoftware@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-28 20:39:44 +0000

    v2.1.3 release - settings up dates - spit*polish
    git-svn-id: https://plugins.svn.wordpress.org/roost-for-bloggers/tags/2.1.3@940152 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ roost.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: Roost Web Push
+ * Plugin URI: http://goroost.com/
+ * Description: Drive traffic to your website with Safari Mavericks push notifications and Roost.
+ * Version: 2.1.3
+ * Author: Roost
+ * Author URI: http://goroost.com
+ * License: GPLv2 or later
+ */
+	
+define( 'ROOST_URL', plugin_dir_url( __FILE__ ) );
+
+require_once( plugin_dir_path( __FILE__ ) . 'includes/class-roost-core.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'includes/class-roost-api.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'includes/class-roost-bbpress.php' );
+
+$roost = new Roost();
+$bbPress_active = Roost_bbPress::bbPress_active();
+
+if( !empty( $bbPress_active['present'] ) && !empty( $bbPress_active['enabled'] ) ) {
+    $roost_bbp = new Roost_bbPress();
+}
+
+register_activation_hook( __FILE__, array( 'ROOST', 'init' ) );
+register_uninstall_hook( __FILE__, array( 'ROOST', 'uninstall' ) );
